8efd2b1932ad15c21668e5fc39e1d1e1d34af249
ambepode <noasly@daum.net>
2017-05-31 15:57:46 +0900

    Modify Session Error

--- application/hooks/SiteBuilder.php
+++ application/hooks/SiteBuilder.php
@@ -3,14 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SiteBuilder extends CI_Controller {
 
+	private $CI;
+
 	public function __construct() {
-		parent::__construct();
-		$this->load->helper(['common']);
+		// & get_instance() : CI 내부 객체를 컨트롤러, 모델, 뷰 이외에서 사용할 수 있도록 하기 위함
+		$this->CI =& get_instance();
+
+		// Check if session lib is loaded or not
+		if(!isset($this->CI->session)){
+			$this->CI->load->library('session');
+		}
 	}
 
 	public function loadHeader() {
 		$css = loadAssets(['/assets/css/site/style.css'], 'css');
-		$view = $this->load->view('header', [
+		$view = $this->CI->load->view('header', [
 			'css' => $css
 		], true);
 
@@ -22,7 +29,7 @@ class SiteBuilder extends CI_Controller {
 			'/assets/js/jquery-3.2.0.js',
 			'/assets/js/common.js'
 		], 'js');
-		$view = $this->load->view('footer', [
+		$view = $this->CI->load->view('footer', [
 			'js' => $js
 		], true);
 
